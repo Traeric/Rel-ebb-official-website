4ad82417f81a7cbb90b8a77a1f5018903bbe12ba
Eric Jin <jxin48598@gmail.com>
2019-08-27 16:56:24 +0800

    chat room

--- front-end/src/components/ChatRoom/ChatRoom.vue
+++ front-end/src/components/ChatRoom/ChatRoom.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        <span>gfgfdgfd</span>
+    <div id="chat_room">
+        <div class="top">
+
+        </div>
+        <div class="left">left</div>
+        <div class="content">main</div>
+        <div class="right">right</div>
+        <div class="footer">footer</div>
     </div>
 </template>
 
@@ -13,6 +19,36 @@ export default {
 
 
 <style scoped lang="stylus">
-
+#chat_room
+    display grid
+    width 100%
+    grid-template-columns repeat(4, 1fr)
+    grid-template-rows repeat(3, 1fr)
+    .top
+        grid-column-start 1
+        grid-column-end 5
+        grid-row-start 1
+        grid-row-end 2
+        height 60px
+    .left 
+        grid-column-start 1
+        grid-column-end 2
+        grid-row-start 2
+        grid-row-end 3
+    .content
+        grid-column-start 2
+        grid-column-end 4
+        grid-row-start 2
+        grid-row-end 3
+    .right 
+        grid-column-start 4
+        grid-column-end 5
+        grid-row-start 2
+        grid-row-end 3
+    .footer 
+        grid-column-start 1
+        grid-column-end 5
+        grid-row-start 3
+        grid-row-end 4
 </style>
 
