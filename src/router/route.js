0487342a1dff4cef1b8da59e1226e8edcaab0967
Eric Jin <jxin48598@gmail.com>
2019-11-22 14:09:52 +0800

    试用github actions

--- src/router/route.js
+++ src/router/route.js
@@ -2,12 +2,6 @@ import vue from "vue";
 import Router from "vue-router";
 import HomePage from "../components/HomePage/HomePage";
 import Download from "../components/Download/Download";
-import ChatRoom from "../components/ChatRoom/ChatRoom";
-import Forum from "../components/Forum/Forum";
-import ForumArticle from "../components/Forum/Article/Article";
-import ForumQuestion from "../components/Forum/Question/Question";
-import ArticleItem from "../components/Forum/Article/Item";
-import Blog from "../components/Blog/Blog";
 
 vue.use(Router);
 
@@ -21,35 +15,6 @@ export default new Router({
             path: "/download",
             component: Download,
             name: "download",
-        },
-        {
-            path: "/chat_room",
-            component: ChatRoom,
-            name: "chat_room",
-        },
-        {
-            path: "/forum",
-            component: Forum,
-            name: "forum",
-            children: [
-                {
-                    path: "article",
-                    component: ForumArticle,
-                },
-                {
-                    path: "article/item/:id",
-                    component: ArticleItem,
-                },
-                {
-                    path: "question",
-                    component: ForumQuestion,
-                },
-            ],
-        },
-        {
-            path: "/blog",
-            component: Blog,
-            name: "blog",
         },
     ],
 });
